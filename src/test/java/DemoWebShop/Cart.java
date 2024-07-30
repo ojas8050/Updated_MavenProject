f7ca6b99575029dae16510da466c379e038afce7
Ojas Chopade <ojas.chopade@zucitech.com>
2024-07-30 18:42:26 +0530

    Updated Lines

--- src/test/java/DemoWebShop/Cart.java
+++ src/test/java/DemoWebShop/Cart.java
@@ -5,12 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Reporter;
 
+import java.time.Duration;
 import java.util.List;
 
-public class Cart {
+public class Cart{
+
     private WebDriver driver;
     private ActionClass action;
+    BrowserActions ba= new BrowserActions(driver);
 
     public Cart(WebDriver driver){
         this.driver=driver;
@@ -39,15 +43,20 @@ public class Cart {
 
     public boolean isItemInCart(String itemName) {
         try {
+            //ExplicitWait((WebElement) By.cssSelector(".cart-item-row"), Duration.ofSeconds(10));
             List<WebElement> cartItems = driver.findElements(By.cssSelector(".product-name"));
 
             for (WebElement item : cartItems) {
+                String itemText = item.getText();
+                Reporter.log(" "+itemText);
+                ba.ExplicitWait(item, Duration.ofSeconds(5));
                 if (item.getText().equalsIgnoreCase(itemName)) {
                     return true;
                 }
             }
             return false;
         } catch (Exception e) {
+            System.out.println(e.getMessage());
             return false;
         }
     }
